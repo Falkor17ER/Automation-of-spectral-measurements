2a58175b9d192ec39a6d1a01a0e9339f8ac935f3
Falkor17ER <eyalrozenwald17@gmail.com>
2023-03-22 13:12:30 +0200

    Creating ''Code Files'' folder

--- build_labtool.bat
+++ build_labtool.bat
@@ -1,4 +1,4 @@
-pyinstaller --noconfirm --onefile --windowed --add-data "C:/Users/2lick/OneDrive - post.bgu.ac.il/Documents/Final BSC Project/Code/Classes;Classes/"  "C:/Users/2lick/OneDrive - post.bgu.ac.il/Documents/Final BSC Project/Code/GUI.py"
+pyinstaller --noconfirm --onefile --windowed --add-data "./Code Files"  "./Code Files/GUI.py"
 
 xcopy /R /Y "connections.json" "dist"
 pause
