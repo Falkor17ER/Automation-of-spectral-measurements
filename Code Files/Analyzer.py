bb786f294aaee69edd1a6a45b5bd6ef920a0bf07
alexdob1995 <alexdb4484@gmail.com>
2023-04-17 22:47:56 +0300

    Added Eyal's normalization function

--- Code Files/Analyzer.py
+++ Code Files/Analyzer.py
@@ -30,19 +30,11 @@ def getNormlizedByRealFreq(dirname, real_freq = '1500'):
 
     divided_df = substance_df.copy()
     divided_df[freqs] = divided_df[freqs].div(clean_df[freqs])
-<<<<<<< HEAD
 
     return divided_df
 
 
     
-=======
-    
-    return divided_df
-
-
-
->>>>>>> origin/InteractiveResults
 def getNormlizedByCustomFreq(dirname, Freq = '1500'):
     # looking for a frequency closest to the users choice
     clean_df = pd.read_csv(dirname+'\\'+'clean.csv', nrows=1)
@@ -50,27 +42,36 @@ def getNormlizedByCustomFreq(dirname, Freq = '1500'):
     freqs = [float(element) for element in columns[10:]]
     distance_from_user = [abs(float(Freq)-element) for element in freqs]
     real_freq = str(freqs[np.argmin(distance_from_user)])
-<<<<<<< HEAD
     return getNormlizedByRealFreq(dirname=dirname, real_freq=real_freq)
     
-
+def getNormlizedAllanVariance(dirname):
+    allan_df = pd.read_csv(dirname+'\\'+'allan.csv')
+    allanNorm_df = allan_df.copy()
+    columns = allan_df.columns.to_list()
+    freqs = [element for element in columns[11:]]
+    #
+    normRow = allan_df.loc[0, columns[11:]].values.flatten().tolist()
+    repName = allan_df.loc[0, columns[4]]
+    powerName = allan_df.loc[0, columns[5]]
+    for index, irow in allan_df.iterrows():
+        if (index != 0):
+            if ( (repName != allan_df.loc[index, columns[4]]) or (powerName != allan_df.loc[index, columns[5]]) ):
+                normRow = allan_df.loc[index, columns[11:]].values.flatten().tolist()
+                repName = allan_df.loc[index, columns[4]]
+                powerName = allan_df.loc[index, columns[5]]
+                # The row (index) stay as it is.
+            else:
+                thisRow = allan_df.loc[index, columns[11:]].values.flatten().tolist()
+                normalizedRow = []
+                for i in range(0, len(thisRow)):
+                    normalizedRow.append(round(thisRow[i]/normRow[i], 2))
+                allanNorm_df.loc[index,freqs] = normalizedRow
+    # The for loop is over. Save all the data.
+    allanNorm_df.to_csv(dirname+'\\allan_norm.csv', index=False, encoding='utf-8')
+    del allan_df
+    del allanNorm_df
 
 if __name__=='__main__':
     now = time.time()
     getNormlizedByCustomFreq("..\\Results\\2023_03_23_15_29_25_116512_Eyal & Alex", '1475')
-=======
-    df = getNormlizedByRealFreq(dirname=dirname, real_freq=real_freq)
-    df.to_csv(dirname+'norm.csv', index=False, encoding='utf-8')
-
-def beerLambertLaw(laserpower=0):
-    concentration = 0
-    #calculateCaptureLightPower()
-    #calculateLaserLightPower(laserpower)
-    #calculateConcentrationOfSubstance()
-    return concentration
-
-if __name__=='__main__':
-    now = time.time()
-    getNormlizedByCustomFreq("..\\Results\\2023_03_23_15_29_25_116512_Eyal & Alex\\", '1475')
->>>>>>> origin/InteractiveResults
     print("normalize and divide time: {:.2f}".format(time.time()-now))
