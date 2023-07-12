d34c5434c49341a232518a07b8840aedbb32080d
Falkor17ER <eyalrozenwald17@gmail.com>
2023-07-12 14:24:48 +0300

    exe file is OK

--- Code Files/OSA.py
+++ Code Files/OSA.py
@@ -1,22 +1,24 @@
+# This file is an API to work and send commands to the OSA device.
 import socket
 from time import sleep, time
 
 class OSA:
-    # Class to operate the OSA
+    # Class to operate the OSA:
+
     def __init__(self, IP):
-        # Create a TCP/IP socket
+        # Create a TCP/IP socket.
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Connect the socket to the port where the server is listening
         server_address = (IP, 10001)
         print('connecting to {} port {}'.format(*server_address))
         self.sock.connect(server_address)
-        self.sock.settimeout(20)
-        self.Auth()
-        self._pts = None
-
+        self.sock.settimeout(120)
+        if self.Auth():
+            self.sendToOSA(':sens:sweep:points?')
+            self._pts = int(self.receiveFromOSA())
 
     def sendToOSA(self, string: str, print_flag = True):
-        # Add a newline character to the end of the string
+        # Add a newline character to the end of the string.
         string = string + "\r\n"
         if print_flag:
             print('sending {}'.format(string))
@@ -26,7 +28,7 @@ class OSA:
         self.sock.sendall(binary_data)
 
     def receiveFromOSA(self, MSGLEN = 1024) -> str:
-        # Add a newline character to the end of the string
+        # Add a newline character to the end of the string.
         binary_data = self.sock.recv(1024)
         string = binary_data.decode("utf-8")
         # Remove the newline character from the end of the string
@@ -34,6 +36,7 @@ class OSA:
         return string
 
     def Auth(self):
+        # Auttentication - the connection is OK.
         self.sendToOSA('open \"anonymous\"') # This is open \"Username"
         if self.receiveFromOSA() != 'AUTHENTICATE CRAM-MD5.':
             print("Connection failed\n")
@@ -50,7 +53,7 @@ class OSA:
                 return True
 
     def setCenterFreq(self, cf):
-        # Set the centeer frequency in nm.
+        # Set the center wavelength/frequency in nm.
         try:
             cf = float(cf)
             if cf < 600 or cf > 1700:
@@ -63,7 +66,7 @@ class OSA:
         return True
 
     def setSpan(self, span):
-        # Set the span. The range will be: [(cf-span/2, cf+span/2)]
+        # Set the span. The range will be: [(cf-span/2, cf+span/2)][nm].
         self.sendToOSA(':sens:wav:span {}nm'.format(span))
         return True
 
@@ -71,27 +74,45 @@ class OSA:
         # The number of points per sweep.
         if points == 'auto on':
             self.sendToOSA(':sens:sweep:points:{}'.format(points))
-            self._pts = 500
         else:
             self.sendToOSA(':sens:sweep:points:auto off')
             self.sendToOSA(':sens:sweep:points {}'.format(points))
-            self._pts = int(points)
+        self.sendToOSA('*CLS')
+        pts_not_updated = True
+        self.sendToOSA(':sens:sweep:points?')
+        while pts_not_updated:
+            try:
+                self._pts = int(self.receiveFromOSA())
+                pts_not_updated = False
+            except:
+                sleep(1)
+                pts_not_updated = True
+                self.sendToOSA(':sens:sweep:points?')
         return True
+    
+    def getPoints(self):
+        # Get from the device the number of points per sample.
+        return self._pts
+    
+    def setAveraging(self, times):
+        # From documantation :SENSE:AVERAGE:COUNT 100.
+        self.sendToOSA(':SENSE:AVERAGE:COUNT {}'.format(times))
 
     def setSpeed(self, speed):
-        # Changing the speed of the sweep
+        # Changing the speed of the sweep.
         self.sendToOSA(':sens:sweep:speed {}'.format(speed))
         return True
 
     def setSens(self, sens):
-        # Changing the sensetivity of the device measurment
+        # Changing the sensetivity of the device measurment.
         self.sendToOSA(':sens:sens {}'.format(sens))
         return True
 
-    ############################################################################## Page 68 on OSA Control Manuall
     def setRes(self, res):
-    # Changing the resolution of the device measurment
-        # self.sendToOSA(''.format(res)) 
+    # Changing the resolution of the device measurment.
+        # self.sendToOSA(':SENSe:BANDwidth|:BWIDth[:RESolution]<wsp><NRf>[M|Hz]'.format(res))
+        self.sendToOSA(':SENSE:BANDWIDTH:RESOLUTION {:.3f}NM'.format(float(res)))
+        #self.sendToOSA(''.format(res))
         return True
 
     def saveBMPFile(self, name='temp'):
@@ -121,8 +142,6 @@ class OSA:
         # number of sweeps
         while mode != 0:
             # mode: number of sweeps
-
-
             self.sendToOSA(':init:smode 1')
             self.sendToOSA('*CLS')
             sweep_completed = False
@@ -136,14 +155,12 @@ class OSA:
                     if ans == '1':
                         sweep_completed = True
                         break
-                    if time() - start > 5:
+                    if time() - start > 30:
                         break
-                                
-                
             mode = mode - 1
     
     def sweepLive(self, mode=1):
-        # number of sweeps
+        # Number of sweeps.
         sleepTime = 0.1
         index = 0
         while mode != 0:
@@ -155,7 +172,6 @@ class OSA:
             sleep(0.05)
             self.sendToOSA(':init')
             sleep(0.05)
-
             # Wating for sweep to complete
             N = 200 * sleepTime
             i = 0
@@ -172,15 +188,24 @@ class OSA:
                 i = i+1
                 if (i >= N):
                     ans = '1'
-
             print(index)
             print(i)    
             mode = mode - 1
 
     def __checkNumberOfPoints(self, data):
+        # This function check if the number of points that were set by the user are taken exactly in the right number. Else it will cause problem and there is a need to execute another measurement.
         data_decoded = data.decode("utf-8")
         data_decoded = data_decoded.split("\r\n")
         len_smpls = len(data_decoded[39:-2])
         if len_smpls == self._pts:
             return True
         return False
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# For Our checking:
+if __name__ == '__main__':
+    osa = OSA(IP = '10.0.0.101')
+    osa.setAveraging('2')
+
+# End of 'OSA.py' file.
