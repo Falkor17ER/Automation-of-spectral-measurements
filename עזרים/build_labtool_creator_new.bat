071b36de1d24c1ed017d7bc109b62c2431a56527
alexdob1995 <alexdb4484@gmail.com>
2023-07-25 12:35:41 +0300

    Clean version

--- עזרים/build_labtool_creator_new.bat
+++
@@ -1,6 +0,0 @@
-pyinstaller --noconfirm --onefile --windowed --add-binary "./Code Files/NKTPDLL.dll;." --add-data "./Code Files/NKTP_DLL.py;." --add-data "./Code Files/connections.json;." --add-data "./Code Files/LASER.py;." --add-data "./Code Files/OSA.py;." --add-data "./Code Files/Operator.py;." --add-data "./Code Files/GUI.py;." --add-data "./Code Files/allantools.py;." --add-data "./Code Files/Analyzer.py;." --add-data "./Code Files/Interactive_Graph.py;." "./Code Files/GUI.py"
-
-copy /Y ".\Code Files\connections.json" "dist"
-copy /Y ".\Code Files\NKTPDLL.dll" "dist"
-rmdir /S /Q build
-pause
